acfd69efbf4f00b533f8eeb7cf0fda1651cebc02
fdr129zh <volador129zh@gmail.com>
2026-04-27 22:43:40 +0800

    refactor: 删除了旧有的 ppmstream/ 目录结构

--- include/ppmstream.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ppmstream/pixel/RGB.hpp"
-#include "ppmstream/pixel/Point.hpp"
-#include "ppmstream/math/Mat.hpp"
-#include "ppmstream/math/Vec.hpp"
-#include "ppmstream/stream/PPMBuffer.hpp"
-#include "ppmstream/stream/PPMStream.hpp"
-#include "ppmstream/stream/PPMDrawer.hpp"
